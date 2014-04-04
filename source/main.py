6115e7a5191234108206157f73adc0c051c2f639
cjallen141 <cjdrumm3r@gmail.com>
2014-04-04 18:28:08 -0400

    added arg read to main.py

--- source/main.py
+++ source/main.py
@@ -2,15 +2,19 @@ import sys
 
 
 
+
+
+def print_usage():
+	print "\nUsage:\n--main.py <torrent_file_name.torrent>"
+	print "--To create .torrent file, call init_torrent.py <file_name>"
+
 def main():
-	#tab
-	print "hello world, bitches!"
+	args = sys.argv[1:]
+	#todo add checking for correct argument input
+	if '--help' in args or len(args)==0:
+		print_usage()
+		sys.exit(2)
 
-
-
-
-
-
-##leave this alone, this is where the program starts
+##program start
 if __name__ == "__main__":
 	main()
