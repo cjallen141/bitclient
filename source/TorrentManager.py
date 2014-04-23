c9a2792e6b0bd34b1ceda1e43c27811b0da6325c
Brenton Yiu <brent.yiu@gmail.com>
2014-04-22 22:40:52 -0400

    Changed TorrentManager

--- source/TorrentManager.py
+++ source/TorrentManager.py
@@ -4,17 +4,13 @@ from TrackerManager import TrackerManager
 from peermanager import PeerManager
 from PieceManager import PieceManager
 import os
+import sys
+import getopt
 #import time
 #import threading
 
 
-def main():
-
-    # use this to enable/disable all the other printing
-
-    print 'Initializing Torrent Manager...',
-
-    data = {}
+def main(argv):
 
     # The first thing we want to do is have them input a file
     # For now this is just going to be hard-coded in
@@ -24,22 +20,65 @@ def main():
     #file1 = '../referenceFiles/TestTorrent.torrent'
     #file1 = '../referenceFiles/WhySoccerMatters-Original.torrent'
     #file1 = 'ThisIsNotARealFile.torrent'
-    file1 = '/Users/brent/Downloads/ubuntu-13.10-desktop-amd64.iso.torrent'
+    #file1 = '/Users/brent/Downloads/ubuntu-13.10-desktop-amd64.iso.torrent'
     #file1 = '/Users/brent/Downloads/t-rice.jpg.torrent'
     #file1 = '../referenceFiles/ProGit.pdf.torrent'
     #file1 = '/Users/brent/Downloads/test.torrent'
 
+    # Defaults
+    file1 = 'bad'
+    output_path = './'
+    port = 61130
+    max_connections = 5
+    udp_first = True
+
+    #print argv
+
+    try:
+        opts, args = getopt.getopt(argv, 'spmf:', ['--help'])
+    except getopt.GetoptError as err:
+        print 'bitclient.py -i <torrentfile>'
+        print 'Optional Arguments'
+        print '\t-o for <outputpath>'
+        print '\t-p for <port>'
+        print '\t-m for <maxconnections>'
+        print '\t--help for Help'
+
+        print err
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-f':
+            file1 = arg
+        elif opt == '-s':
+            output_path = arg
+        elif opt == '-p':
+            port = int(arg)
+        elif opt == '-m':
+            max_connections = arg
+        else:
+            print 'bitclient.py -i <torrentfile>'
+            print 'Optional Arguments'
+            print '\t-o for <outputpath>'
+            print '\t-p for <port>'
+            print '\t-m for <maxconnections>'
+            print '\t--help for Help'
+
+    print 'Initializing Torrent Manager...',
+
+    data = {}
+
     data['torrent_file'] = file1
-    data['output_path'] = '/Users/brent/Documents'
-    data['peer_id'] = 'ThisIsATestOfGTENS00'
+    data['output_path'] = output_path
+    data['peer_id'] = 'BrentonChrisBitTorrt'
     #data['key'] = '50EDCACE'
     data['key'] = 'A89BA1C1'
-    data['port'] = 61130
+    data['port'] = port
     data['compact'] = 1
     data['no_peer_id'] = 1
-    data['max_connections'] = 10
+    data['max_connections'] = max_connections
     data['state'] = 'started'
-    data['numwant'] = 20
+    data['numwant'] = -1
     data['file_name'] = []
     data['length'] = []
     data['file_length'] = 0
@@ -63,9 +102,16 @@ def main():
     # Add our stuff to the data structure
     data['info_hash'] = info_hash
     data['piece_length'] = decoded_data['info']['piece length']
-    data['announce'] = decoded_data['announce']
+    data['announce'] = [decoded_data['announce']]
     data['pieces_hash'] = decoded_data['info']['pieces']
     data['name'] = decoded_data['info']['name']
+    data['udp_first'] = udp_first
+
+    for link in decoded_data['announce-list']:
+        url = link[0]
+        data['announce'].append(url)
+
+    print data['announce']
 
     # Check to see if it is a multi-file torrent or a single-file torrent
     # Multi File
@@ -117,4 +163,4 @@ def main():
 
 if __name__ == "__main__":
 
-    main()
+    main(sys.argv[1:])
